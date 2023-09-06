916d152ee2fd5a1ade428d3e45bf4ca6cb07f356
dirty <dirty@localhost.localdomain>
2023-09-06 05:51:04 +0300

    derp

--- code/main.c
+++ code/main.c
@@ -2,27 +2,58 @@
 
 
 
+#define printf   	( (int(FNC*)(char*,...)) 0x800f6f10 )
 
 
 
 
-uint f_80101244_midi_juice_stuff(byte *midiJuice,int *param_2)
+
+
+
+
+
+uint f_80101244_midi_juice_stuff(byte *midiJuice,int *out)
 
 {
-  uint uVar1;
-  int iVar2;
+  uint u1;
+  int i1;
   
-
-  uVar1 = (uint)*midiJuice;
-  iVar2 = 1;
+  u1 = (uint)*midiJuice;
+  i1 = 1;
   if ((*midiJuice & 0x80) != 0) {
-    uVar1 = uVar1 & 0x7f;
+    u1 = u1 & 0x7f;
     do {
       midiJuice = midiJuice + 1;
-      iVar2 = iVar2 + 1;
-      uVar1 = uVar1 * 0x80 + (*midiJuice & 0x7f);
+      i1 = i1 + 1;
+      u1 = u1 * 0x80 + (*midiJuice & 0x7f);
     } while ((*midiJuice & 0x80) != 0);
   }
-  *param_2 = iVar2;
-  return uVar1;
+  *out = i1;
+  return u1;
 }
+
+
+
+
+void f_80101838_midi_help1(HeadMID *mid)
+{
+  HeadMID *pp;
+  int i1;
+  
+  printf("help1 %08X\n", mid);
+  
+  mid->f_40_unkunk = 0;
+  i1 = 0;
+  pp = mid;
+  do {
+    *(undefined *)((int)mid->f_64_arr_a + i1 + -0x20) = 0;
+    *(undefined *)((int)mid->f_64_arr_a + i1 + -0x10) = 0x7f;
+    pp->f_64_arr_a[0] = 0;
+    pp->f_84_arr_b[0] = 0x2000;
+    i1 = i1 + 1;
+    pp = (HeadMID *)((int)&pp->f_00_sign + 2);
+  } while (i1 < 0x10);
+  mid->f_42_msk1 = 0;
+  return;
+}
+
--- code/types.h
+++ code/types.h
@@ -17,6 +17,8 @@ typedef unsigned char uchar;
 typedef unsigned long ulong;
 typedef unsigned int uint; 
 
+#define FNC
+
 //----------------------------------------
 
 
@@ -45,6 +47,51 @@ typedef struct MATRIX {
     short m[9];
     long t[3];
 } MATRIX;
+
+
+//--------------------------------
+
+
+typedef struct HeadMID HeadMID;
+
+
+
+struct HeadMID {
+    uint f_00_sign;
+    ushort f_04_s1;
+    byte f_06_s2;
+    byte f_07_s3;
+    int f_08_u;
+    HeadMID * f_0c_next;
+    uint f_10_name;
+    byte * f_14_ptr_sbnk;
+    byte * f_18_juice_start;
+    byte * f_1c_rt_juice_cur;
+    byte * f_20_ptr_MMID;
+    int f_24_dunno1;
+    int f_28_unk1;
+    uint f_2c_spd_rel;
+    uint f_30_iii2;
+    int f_34_unk2;
+    short f_38_s;
+    short f_3a_ww;
+    short f_3c_unk3;
+    short f_3e_yyy;
+    short f_40_unkunk;
+    ushort f_42_msk1;
+    int field22_0x44;
+    int field23_0x48;
+    int field24_0x4c;
+    int field25_0x50;
+    int field26_0x54;
+    int field27_0x58;
+    int field28_0x5c;
+    int field29_0x60;
+    short f_64_arr_a[16];
+    short f_84_arr_b[16];
+    HeadMID * f_a4_ptr_ano_mid;
+    byte * f_a8_ptr;
+};
 
 
 
